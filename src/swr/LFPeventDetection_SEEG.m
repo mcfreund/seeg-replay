873b9ca8aaa05486809a20b7f5073bf155cdf05e
amjaskir <amjaskir@gmail.com>
2024-02-08 15:46:48 -0500

    handle unfiltered data and clean up code

--- src/swr/LFPeventDetection_SEEG.m
+++ src/swr/LFPeventDetection_SEEG.m
@@ -12,32 +12,37 @@ restoredefaultpath;
 addpath(genpath(cat(2,SET_github_root,'/vandermeerlab/code-matlab/shared'))); % clone vandermeerlab repo at https://github.com/vandermeerlab/vandermeerlab
 cd(SET_data_root)
 
+
 %% set things up
 data_file = '/oscar/home/ajaskir/brainstorm_2024/seeg-replay/src/swr/ca1_data_matrix.mat';
 load(data_file)
 
-% pick one electrode for now
-test_data = data.e0010GP.CMHIP2.ca1_contact;
-lfp = test_data;
-lfp.cfg.history.mfun = {};   % filler
-lfp.cfg.history.cfg = {};    % filler
+%% pick one electrode for now
+which_contact = data.e0010GP.CMHIP2.ca1_contact;
+test_data = which_contact;                      % "data" field specifies band pass filtered
+%test_data.data = test_data.data_unfiltered;     % use unfiltered LFP instead
 
-%%
+lfp_raw = test_data;
+lfp_raw.cfg.history.mfun = {};   % filler
+lfp_raw.cfg.history.cfg = {};    % filler
+
+%% Vanilla power and z-scoring (from LFPDetection)
 % create tsd object that contains the fields specified in
 % /vandermeerlab/code-matlab/shared/datatypes/tsd/CheckTSD.m
 
-lp = LFPpower([],lfp);
-lgp_z = zscore_tsd(lp);
+cfg = []; cfg.output = 'power';
+lfp_power = LFPpower([],lfp_raw); % what is this spitting out? time x power 
+lfp_power_z = zscore_tsd(lfp_power);
 
 %% detect events
 cfg = [];
 cfg.method = 'raw';
-cfg.threshold = 3;
+cfg.threshold = 3; %3
 cfg.dcn =  '>'; % return intervals where threshold is exceeded
 cfg.merge_thr = 0.05; % merge events closer than this
 cfg.minlen = 0.05; % minimum interval length
 
-lgp_evt = TSDtoIV(cfg,lgp_z);
+lgp_evt = TSDtoIV(cfg,lfp_power_z);
 
 %% to each event, add a field with the max z-scored power (for later selection)
 cfg = [];
@@ -45,7 +50,7 @@ cfg.method = 'max'; % 'min', 'mean'
 cfg.label = 'maxlgp'; % what to call this in iv, i.e. usr.label
 cfg.label = 'maxSWRpower_z'; % what to call this in iv, i.e. usr.label
 
-lgp_evt = AddTSDtoIV(cfg,lgp_evt,lgp_z);
+lgp_evt = AddTSDtoIV(cfg,lgp_evt,lfp_power_z);
 
 %% select only those events of >5 z-scored power
 cfg = [];
@@ -55,20 +60,15 @@ cfg.threshold = 5;
 lgp_evt = SelectIV(cfg,lgp_evt,'maxSWRpower_z');
 
 %% plot events in highlighted on top of full lfp
-PlotTSDfromIV([],lgp_evt,lfp);
+PlotTSDfromIV([],lgp_evt,test_data);
 
-%% ..or the events alone (fixed 200ms window centered at event time)
+%% plot the events alone (fixed 200ms window centered at event time)
 close all;
 
 cfg = [];
 cfg.display = 'iv';
 cfg.mode = 'center';
-cfg.fgcol = 'k';
-
-PlotTSDfromIV(cfg,lgp_evt,lfp);
-%% ..hold on (highlight edges of event on top of previous plot)
-cfg = [];
-cfg.display = 'iv';
+cfg_def.width = 1.0;
 cfg.fgcol = 'r';
 
-PlotTSDfromIV(cfg,lgp_evt,lfp);
+PlotTSDfromIV(cfg,lgp_evt,test_data);
