658776098b9107b40d5624ad1ea446f76b8d9c45
Dan <dnscott87@gmail.com>
2024-03-09 20:21:15 -0800

    Fix swr-behavior plots for report
    - fix up sizes, axes, tight-layout
    - toggle save and sessions back on in src/swr/run.py

--- src/swr/plots.py
+++ src/swr/plots.py
@@ -1,78 +1,99 @@
-from matplotlib.pyplot import pyplot as plt
+import matplotlib.pyplot as plt
 
 def plot_swr(i, swrs, lfp_bp, pow_z, contact):
+
     plt.figure(figsize=[8,6])
+    plt.suptitle(f'e0010GP Encoding and SameDayRecall\nContact {contact} SWR {i}')
+
     plt.subplot(3,1,1)
     plt.plot(swrs.ctime[i], swrs.ctxts[i])
     plt.plot(swrs.times[i], swrs.traces[i])
-    
+    plt.ylabel('LFP [V]')
+
     plt.subplot(3,1,2)
     plt.plot(swrs.ctime[i], lfp_bp[ swrs.ctx_beg[i]:swrs.ctx_end[i] ])
     plt.plot(swrs.times[i], lfp_bp[ swrs.idx_beg[i]:swrs.idx_end[i] ])
+    plt.ylabel('Band-Passed LFP, 80-100 Hz')
 
     plt.subplot(3,1,3)
     plt.plot(swrs.ctime[i], pow_z[ swrs.ctx_beg[i]:swrs.ctx_end[i] ])
     plt.plot(swrs.times[i], pow_z[ swrs.idx_beg[i]:swrs.idx_end[i] ])
-    
-    plt.title(f'Contact {contact} SWR {i} ')
+    plt.ylabel('Z-scored Power')
+    plt.xlabel('Time [s]')
+
     plt.tight_layout()
 
 def plot_swrs_and_behavior(df):
+    # 
+    plt.ion()
+
+    figsize = [5,4]
+
     # Encoding counts vs performance
-    plt.figure()
-    plt.plot(df['swr_cnt_enc'], df['err_pos_enc'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_enc'], abs(df['err_pos_enc']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP Encoding SWR by Encoding Accuracy')
+    plt.tight_layout()
 
-    plt.figure()
-    plt.plot(df['swr_cnt_enc'], df['err_pos_same'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_enc'], abs(df['err_pos_same']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP Encoding SWR by SameDay Accuracy')
+    plt.tight_layout()
 
-    plt.figure()
-    plt.plot(df['swr_cnt_enc'], df['err_pos_next'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_enc'], abs(df['err_pos_next']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP Encoding SWR by NextDay Accuracy')
+    plt.tight_layout()
 
-    plt.figure()
+    plt.figure(figsize=figsize)
     diff1 = abs(df['err_pos_enc'])-abs(df['err_pos_same'])
     diff2 = abs(df['err_pos_enc'])-abs(df['err_pos_next'])
     plt.plot(df['swr_cnt_enc']-0.05, diff1,'o')
     plt.plot(df['swr_cnt_enc']+0.05, diff2,'o')
+    plt.tight_layout()
 
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error Change')
     plt.title('SWR Counts vs Improvement')
     plt.legend(['Encoding vs Same', 'Encoding vs Next'])
+    plt.tight_layout()
 
 
     # Same-day counts vs performnace
-    plt.figure()
-    plt.plot(df['swr_cnt_same'], df['err_pos_same'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_same'], abs(df['err_pos_same']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP SameDay SWR by SameDay Accuracy')
+    plt.tight_layout()
 
-    plt.figure()
-    plt.plot(df['swr_cnt_same'], df['err_pos_next'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_same'], abs(df['err_pos_next']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP SameDay SWR by NextDay Accuracy')
+    plt.tight_layout()
 
     # Next-day counts vs performance
-    plt.figure()
-    plt.plot(df['swr_cnt_next'], df['err_pos_next'],'o')
+    plt.figure(figsize=figsize)
+    plt.plot(df['swr_cnt_next'], abs(df['err_pos_next']),'o')
     plt.xlabel('Num. SWRs'); plt.ylabel('Position Error')
     plt.title('e0010GP NextDay SWR by NextDay Accuracy')
+    plt.tight_layout()
 
 
     # SWR counts by trial and condition
-    plt.figure()
+    plt.figure(figsize=figsize)
     plt.plot(df['trial'], df['swr_cnt_enc'],'o')
     plt.xlabel('Trial Number')
     plt.ylabel('SWR Count')
     plt.title('e0010GP Encoding SWR Count by Trial')
+    plt.tight_layout()
 
-    plt.figure()
+    plt.figure(figsize=figsize)
     plt.plot(df['condition'], df['swr_cnt_enc'],'o')
     plt.xlabel('Condition Number')
     plt.ylabel('SWR Count')
     plt.title('e0010GP Encoding SWR Count by Condition')
+    plt.tight_layout()
 
--- src/swr/run.py
+++ src/swr/run.py
@@ -2,17 +2,15 @@ from src.shared.paths      import *
 from src.shared.utils      import *
 from src.swr.functions_ca1 import *
 from src.swr.functions_swr import *
-import ipdb
-import dill
 
 read     = False
 paths    = PathPresets('dan-xps-15')
 subjs    = ['e0010GP']#, 'e0011XQ', 'e0013LW', 'e0015TJ', 'e0016YR', 'e0017MC']
-sessions = ['Encoding']#,'SameDayRecall','NextDayRecall']
+sessions = ['Encoding', 'SameDayRecall','NextDayRecall']
 
 # Get raw data from CA1 and assoc. channels, process to SWRs
 lfps = dill_read('./data/ca1_lfps.pt') if read else get_lfp_data(paths, sessions, subjs, save_pkl=False)
-swrs = dill_read('./data/ca1_swrs.pt') if read else get_swrs_from_lfps(lfps, save_pkl=False, save_swrs=False)
+swrs = dill_read('./data/ca1_swrs.pt') if read else get_swrs_from_lfps(lfps, save_pkl=False, save_swrs=True)
 
 # Get raw data from CA1 and assoc. channels, process to SWRs
 #get_swr_bp_lfp_power_z(paths, subjs, sessions)
